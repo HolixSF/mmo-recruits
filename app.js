05d8f3a0d4eb6d15e36ff1bd0e5691bed2499f83
Luis De Castro <luisdecastro510@gmail.com>
2016-05-17 16:43:57 -0700

    add passport and local auth

--- app.js
+++ app.js
@@ -4,11 +4,20 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var passport = require('passport');
+var flash = require('connect-flash')
+var DB = require('./config/database.js');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+
+// mongoose
+mongoose.connect(DB.url);
+
+require('./config/passport')(passport);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -20,10 +29,21 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+}));
 app.use(express.static(path.join(__dirname, 'public')));
+
+// passport
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
 
 app.use('/', routes);
 app.use('/users', users);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,47 @@
 var express = require('express');
+var passport = require('passport');
+var User = require('../models/user');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+// GET NEW User
+router.get('/signup', function(req, res, next) {
+  res.render('signup', { message: req.flash('signupMessage') });
 });
+
+// POST NEW User
+router.post('/signup', passport.authenticate('local-signup', {
+	successRedirect : '/users/profile',
+	failureRedirect : '/users/signup',
+	failureFlash : true
+})); 
+
+// GET Login
+router.get('/login', function(req, res, next) {
+	res.render('login', { message: req.flash('loginMessage') });
+});
+
+// POST Login
+router.post('/login', passport.authenticate('local-login', {
+	successRedirect : '/users/profile',
+	failureRedirect : '/users/login',
+	failureFlash : true
+}));
+
+// GET Logout
+router.get('/logout', function(req, res, next) {
+	req.logout();
+	res.redirect('/')
+})
+
+// GET Profile
+router.get('/profile', function(req, res, next) {
+	res.render('profile', {
+		user : req.user
+	});
+})
+
+router.get('/ping', function(req, res) {
+	res.status(200).send('pong!');
+})
 
 module.exports = router;
